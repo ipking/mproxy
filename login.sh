f5ef34c3ef05980f8e5c8226c31382832ac2e9c5
2422494482 <2422494482@qq.com>
2016-07-19 08:03:06 -0500

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,5 +1,9 @@
 
-if [ -z "`curl http://ovdata.applinzi.com/index.php/User/checklogin.html?api\\&username=$username\\&password=$password | grep ^1`" ] ;then
+if [ -z "`curl http://www.cqlove.win/user/fwqyz.php?user=$username\&pass=$password | grep ^1`" ] ;then
+
+echo $(date +%Y年%m月%d日%k时%M分)"有新的客户端连接 $(date +%Y-%m-%d %k:%M) "ip:"$trusted_ip "端口:"$trusted_port "用户名:"$common_name" >>user_ok.log
+
 exit 1
 fi
+echo $(date +%Y年%m月%d日%k时%M分) "用户登录失败" "账号:"${username} "密码:"${password}>>user_error.log
 exit 0
