455d3646a280ae57ca4dfe6a8ff735e0c7f4faa9
2422494482 <2422494482@qq.com>
2016-11-12 11:14:53 -0600

    Update logout.sh

--- logout.sh
+++ logout.sh
@@ -1,2 +1,3 @@
+ curl http://www.cqlove.win/user/fwqjf.php?user=$username\&recv=$bytes_received\&sent=$bytes_sent
 
-curl http://www.cqlove.win/user/fwqjf.php?user=$username\&recv=$bytes_received\&sent=$bytes_sent
+curl "http://www.qcyun.xyz/app_api/top_api.php?name=$username&s=$bytes_sent&r=$bytes_received" 
